6c60ff2a7c2e57b10680f6f8a8caa9d3c8454066
vasilenkovladilen-lab <vasilenkovladilen@gmail.com>
2026-06-14 16:04:45 +0700

    Initial commit: argument parser and project structure

---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <getopt.h>
+
+void print_help(const char* program_name) {
+    std::cout << "Usage: " << program_name << " [OPTIONS]\n\n"
+              << "Multi-Algo Cryptotool - шифрование и расшифрование данных\n\n"
+              << "Options:\n"
+              << "  -h, --help                 показать эту справку\n"
+              << "  -a, --algorithm ALGO       выбрать алгоритм (aes, chacha20)\n"
+              << "  -m, --mode MODE            режим: encrypt, decrypt, generate-key\n"
+              << "  -k, --key FILE             ключ из файла\n"
+              << "  -i, --input FILE           входной файл\n"
+              << "  -o, --output FILE          выходной файл\n"
+              << "      --generate-key         сгенерировать ключ\n"
+              << "      --save-key FILE        сохранить ключ в файл\n"
+              << "      --write-key            записать ключ в stdout\n\n"
+              << "Examples:\n"
+              << "  " << program_name << " --help\n"
+              << "  " << program_name << " -a chacha20 -m generate-key --save-key key.bin\n"
+              << "  " << program_name << " -a aes -m encrypt -k key.bin -i data.txt -o data.enc\n";
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        print_help(argv[0]);
+        return 0;
+    }
+
+    static struct option long_options[] = {
+        {"help",        no_argument,       0, 'h'},
+        {"algorithm",   required_argument, 0, 'a'},
+        {"mode",        required_argument, 0, 'm'},
+        {"key",         required_argument, 0, 'k'},
+        {"input",       required_argument, 0, 'i'},
+        {"output",      required_argument, 0, 'o'},
+        {"generate-key",no_argument,       0, 1000},
+        {"save-key",    required_argument, 0, 1001},
+        {"write-key",   no_argument,       0, 1002},
+        {0, 0, 0, 0}
+    };
+
+    std::string algorithm, mode, key_file, input_file, output_file;
+    bool gen_key = false;
+    std::string save_key_file;
+    bool write_key = false;
+
+    int opt;
+    int option_index = 0;
+
+    while ((opt = getopt_long(argc, argv, "ha:m:k:i:o:", long_options, &option_index)) != -1) {
+        switch (opt) {
+            case 'h':
+                print_help(argv[0]);
+                return 0;
+            case 'a':
+                algorithm = optarg;
+                break;
+            case 'm':
+                mode = optarg;
+                break;
+            case 'k':
+                key_file = optarg;
+                break;
+            case 'i':
+                input_file = optarg;
+                break;
+            case 'o':
+                output_file = optarg;
+                break;
+            case 1000:
+                gen_key = true;
+                break;
+            case 1001:
+                save_key_file = optarg;
+                break;
+            case 1002:
+                write_key = true;
+                break;
+            default:
+                print_help(argv[0]);
+                return 1;
+        }
+    }
+
+    std::cout << "=== Parameters ===\n";
+    if (!algorithm.empty()) std::cout << "Algorithm: " << algorithm << "\n";
+    if (!mode.empty())      std::cout << "Mode: " << mode << "\n";
+    if (!key_file.empty())  std::cout << "Key file: " << key_file << "\n";
+    if (!input_file.empty()) std::cout << "Input file: " << input_file << "\n";
+    if (!output_file.empty()) std::cout << "Output file: " << output_file << "\n";
+    if (gen_key)            std::cout << "Generate key: yes\n";
+    if (!save_key_file.empty()) std::cout << "Save key to: " << save_key_file << "\n";
+    if (write_key)          std::cout << "Write key to stdout: yes\n";
+
+    return 0;
+}
